e9337e5292c8c1020c913257139983f408828cbc
liuziyi219 <49853156+liuziyi219@users.noreply.github.com>
2025-04-08 12:28:24 -0700

    Add files via upload

--- run_exps_open.py
+++ run_exps_open.py
@@ -5,32 +5,9 @@ from openai import OpenAI
 import json
 import argparse
 from pydantic import BaseModel 
-# from vllm import LLM, SamplingParams
-from prompts_exps import * 
 from tqdm import tqdm
-open_source_models = ['Qwen/Qwen2.5-7B-Instruct',
-        'Qwen/Qwen2.5-14B-Instruct',
-        'Qwen/Qwen2.5-32B-Instruct',
-        'Llama-3.1-8B-Instruct',
-        '/home/ziyi/LLaMA-Factory/output/qwen2.5_7b_lora_sft_5',
-        '/home/ziyi/LLaMA-Factory/output/llama3.2_3b_lora_sft',
-        '/home/ziyi/LLaMA-Factory/output/qwen2.5_14b_lora_sft_5',
-        '/home/ziyi/LLaMA-Factory/output/llama3.2_3b_lora_sft_attitude',
-        '/home/ziyi/LLaMA-Factory/output/qwen2.5_7b_lora_sft_5_attitude',
-        '/home/ziyi/LLaMA-Factory/output/qwen2.5_14b_lora_sft_5_attitude',
-        'meta-llama/Llama-3.1-70B-Instruct',
-        'meta-llama/Llama-3.2-3B-Instruct',
-        "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
-        "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
-        "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"]
-api_models = ['gemini-2.0',
-        'deepseek-v3',
-        'deepseek-r1',
-        'o3-mini',
-        'o1',
-        'gpt-4o-mini',
-]
-
+from prompts_exps import * 
+from vllm import LLM, SamplingParams
 class Response(BaseModel):
     theme1: str
     theme2: str 
@@ -41,25 +18,36 @@ class Response(BaseModel):
 class Value(BaseModel):
     val: str 
 
-api_key = "Your API_KEY"
+api_key = os.environ.get("API_KEY")
+api_key_ds = os.environ.get("API_KEY")
 MODEL_generation = 'o3-mini'
 MODEL_evaluation = 'gpt-4o'
 client = OpenAI(api_key = api_key)
+client_ds = OpenAI(api_key = api_key_ds,base_url="https://api.deepseek.com")
 
-def call_gpt(prompt, model):
-    
-    completion_model = client.chat.completions.create(
-    model = model,
-    stream = False,
-    messages=[
-        {"role": "system", "content": f"You are an expert at understanding cultural values."}, 
-        {"role": "user", "content": prompt}  
-    ]
-    )
-
+def call_model(prompt, model):
+    if model.startswith('DeekSeek'):
+       
+        completion_model = client_ds.chat.completions.create(
+        model = model,
+        stream = False,
+        messages=[
+            {"role": "system", "content": f"You are an expert at understanding cultural values."}, 
+            {"role": "user", "content": prompt}  
+        ]
+        )
+    else:
+        completion_model = client_ds.chat.completions.create(
+        model = model,
+        stream = False,
+        messages=[
+            {"role": "system", "content": f"You are an expert at understanding cultural values."}, 
+            {"role": "user", "content": prompt}  
+        ]
+        )
     result = completion_model.choices[0].message.content
+    
     return result
-
 
 def parse_response(resp, type='response'):
     start = resp.index('{')
@@ -77,123 +65,103 @@ def rephrase_values(values, topic):
     values = values.split("\n")
     for v in values:
         v_prompt = rephrase_prompt.format(value=v) 
-        response = call_gpt(v_prompt, MODEL_generation) 
+        response = call_model(v_prompt, MODEL_generation) 
         val = parse_response(response, 'value')
         values_rephrased.append(val)
 
     return values_rephrased
 
-if __name__=='__main__':
-
+def main():
     parser = argparse.ArgumentParser(
                     prog='Openended-CI')   
-    parser.add_argument('-s', '--stories', default='datasets/dataset_random_original.json')
+    
+    parser.add_argument('-c', '--category', default='human',choices = ['random','ethical','political','religious','social','human'])
     parser.add_argument('-v', '--valueType', default='ethics, social, religion, politics, security, migration and science') 
-    parser.add_argument('-out', '--outFile', default="random_32b_updated_1.csv")
-    parser.add_argument('-m','--model', default="Qwen/Qwen2.5-32B-Instruct")
+    parser.add_argument('-m','--model',default='o1',choices=['gpt-4o-mini','o3-mini','o1','deepseek-chat','deepseek-reasoner','Qwen/Qwen2.5-32B-Instruct'])
+    # parser.add_argument('-out', '--outFile', default="results/o3-mini/results_open_random.csv")
     args = parser.parse_args()
-    outfile = os.path.join('results',args.model,f'results_open_{category}.csv')
-   
+    category = args.category
+    model = args.model
+    outfile = os.path.join('results',args.model,f'results_open_{category}_fixed.csv')
     # read data 
-    file = open(args.stories)
-    data = json.load(file)
-    model_name = args.model
-    results_file = pd.DataFrame()
-    if model_name in open_source_models:
-        if model_name in ['meta-llama/Llama-3.3-70B-Instruct']:
 
-            llm = LLM(model=model_name,trust_remote_code=True,tensor_parallel_size=4)
-        elif model_name in ['Qwen/Qwen2.5-32B-Instruct']:
-            llm = LLM(model=model_name,trust_remote_code=True,tensor_parallel_size=2)
-        else:
-            llm = LLM(model=model_name,trust_remote_code=True)
-        sampling_params = SamplingParams(temperature=0.8,max_tokens=4096)
-        
+    file = f'datasets/dataset_{category}_original.json'
+    data = json.load(open(file))
+
+    results_file = pd.DataFrame()
+
     scores = []
     total_values = []
     original_values = []
-    reasoning = [] 
+    final_judges = [] 
+    summary_judges = []
     gt_resolved = []
     predictions = [] 
-    final_judges = []
-    all_multi_step_prompt_p1=[]
-    for i, _ in enumerate(data): 
+    
+    if model!='Qwen/Qwen2.5-32B-Instruct':
+        for i, _ in tqdm(enumerate(data)):       
+            story = _['story']
+            p1 = multi_step_prompt_p1.format(story=story, topic=args.valueType)
+            response = call_model(p1, args.model)
+
+            summary = response 
+            if category not in ["random","human"] :
+                valueType = category
+            else:
+                valueType = args.valueType
+            p2 = multi_step_prompt_p2.format(story=story, topic=valueType, summary=summary)
+            preds = call_model(p2, args.model) 
+
+            gt_values = _['values']
+            original_values.append(gt_values)
+        
+            predictions.append(preds)
+    else:
+        llm = LLM(model=model,trust_remote_code=True)
+        sampling_params = SamplingParams(temperature=0.8,max_tokens=4096)
+        all_multi_step_prompt_p1=[]
+        for i, _ in enumerate(data): 
         # print(len(data))
-        story = _['story']
-        p1 = multi_step_prompt_p1.format(story=story, topic=args.valueType)
-        all_multi_step_prompt_p1.append([
-        {"role": "system", "content": f"You are an expert at understanding cultural values."}, 
-        {"role": "user", "content": p1}  
-    ])
-    all_summary_response = llm.chat(messages=all_multi_step_prompt_p1,
-                    sampling_params=sampling_params,
-                    use_tqdm=True)
-    all_summary = [output.outputs[0].text for output in all_summary_response]
-    
-    all_multi_step_prompt_p2=[]
-    for d,summary in tqdm(zip(data,all_summary)):
-        # response = call_gpt(p1, MODEL_generation)
-        # we are going to get the summary 
-        # summary = (response[response.index("\"summary\":")+11:-6]) 
-        # summary = response 
-        story = d['story']
-        p2 = multi_step_prompt_p2.format(story=story, topic=args.valueType, summary=summary)
-        all_multi_step_prompt_p2.append([
-        {"role": "system", "content": f"You are an expert at understanding cultural values."}, 
-        {"role": "user", "content": p2}  
-    ])
-    all_results_response = llm.chat(messages=all_multi_step_prompt_p2,
-                    sampling_params=sampling_params,
-                    use_tqdm=True)
-    all_results = [output.outputs[0].text for output in all_results_response]
-        # preds = call_gpt(p2, MODEL_generation) 
-        # print(preds)
-    # use LLM as judge to decide how correct this is. 
-    i=0
-    for _,pred in tqdm(zip(data,all_results)):
-        i+=1
-        gt_values = _['values']
-        original_values.append(len(gt_values)) 
-        # contr = _['contradiction'] 
-        # p3 = resolve_values.format(values=gt_values, contradiction=contr)
-        # response = call_gpt(p3, MODEL_generation)
-        # print(response)
+            story = _['story']
+            if category!="random":
+                valueType = category
+            else:
+                valueType = args.valueType
+            p1 = multi_step_prompt_p1.format(story=story, topic=args.valueType)
+            all_multi_step_prompt_p1.append([
+            {"role": "system", "content": f"You are an expert at understanding cultural values."}, 
+            {"role": "user", "content": p1}  
+        ])
+        all_summary_response = llm.chat(messages=all_multi_step_prompt_p1,
+                        sampling_params=sampling_params,
+                        use_tqdm=True)
+        all_summary = [output.outputs[0].text for output in all_summary_response]
+        
+        all_multi_step_prompt_p2=[]
+        for d,summary in tqdm(zip(data,all_summary)):
+        
+            story = d['story']
+            p2 = multi_step_prompt_p2.format(story=story, topic=args.valueType, summary=summary)
+            all_multi_step_prompt_p2.append([
+            {"role": "system", "content": f"You are an expert at understanding cultural values."}, 
+            {"role": "user", "content": p2}  
+        ])
+        all_results_response = llm.chat(messages=all_multi_step_prompt_p2,
+                        sampling_params=sampling_params,
+                        use_tqdm=True)
+        all_results = [output.outputs[0].text for output in all_results_response]
 
-        # finally judge 
-        # p4 = llm_judge.format(gt=gt_values, pred=pred)
-        # final_judge = call_gpt(p4, MODEL_generation)
+        for _,pred in tqdm(zip(data,all_results)):
 
-        predictions.append(pred)
-        gt_resolved.append(gt_values)
-        # p4 = llm_judge.format(gt=gt_values, pred=pred)
-        # final_judge = call_gpt(p4, MODEL_evaluation)
-        # final_judges.append(final_judge)
-        # predictions.append(pred)
-        # gt_resolved.append(response)
-        # print(final_judge)
+            gt_values = _['values']
+            original_values.append(len(gt_values)) 
+            predictions.append(pred)
+            gt_resolved.append(gt_values)
 
-        # try: 
-        #     # parse final_judge 
-        #     start = final_judge.index('{')
-        #     end = final_judge.index('}')
-        #     final_judge = final_judge[start:end+1]
-        #     print(final_judge)
-        #     final_judge = json.loads(final_judge)
-        #     scores.append(final_judge['score'])
-        #     reasoning.append(final_judge['reasoning'])
-        #     total_values.append(final_judge['len_ground_truth'])
-
-        # except: 
-        #     scores.append(final_judge) 
-        #     reasoning.append(final_judge)
-        #     total_values.append(final_judge)
-
-        # if i == 50: 
-        #     break
-    
-    # results_file['reasoning'] = final_judges 
-    # results_file['total values'] = total_values 
-    # results_file['original total values'] = original_values
     results_file['predictions'] = predictions
-    results_file['ground truth values'] = gt_resolved
+    results_file['ground truth values'] = original_values
     results_file.to_csv(outfile, index=False)
+
+if __name__=='__main__':
+
+    main()
